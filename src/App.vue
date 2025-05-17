ff516adb1df8515d8897e1f1ddfcbfbcb3cb3932
Dio Arayan <drarayan1994@gmail.com>
2025-05-17 08:12:42 +0800

    ClockTimer and TeamName component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,14 @@
 <script setup>
+import ClockTimer from './components/ClockTimer.vue';
 import Counter from './components/Counter.vue'
 </script>
 
 <template>
-  <main>
-    <Counter />
-    <Counter />
+  <main class="max-w-full m-auto p-5 font-sans">
+    <div class="flex justify-center">
+      <ClockTimer />
+      <Counter team-name="Home" />
+      <Counter team-name="Visitor" />
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,8 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    tailwindcss(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
